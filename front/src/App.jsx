5134d9c6764be7db4ad6d173633c58cf8473e63f
ライガー <44018535+raiga0310@users.noreply.github.com>
2024-02-28 08:59:06 +0000

    fix: secrets->.envにへんこう

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { loadPrtmiesToken } from './utils/loadToken';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
--- front/src/utils/loadToken.js
+++ front/src/utils/loadToken.js
@@ -1,20 +1,7 @@
-import fs from "node:fs";
-
-const SECRET_TOKENS = '/run/secrets/secret_tokens';
-
-const re_prtimes = /prtimes=?<token>\w+/;
-const re_mapbox = /mapbox=?<token>\w+/;
-
 export function loadPrtmiesToken() {
-    const data = fs.readFileSync(SECRET_TOKENS, 'utf8').trim();
-    const { token } = re_prtimes.exec(data).groups;
-
-    return token
+    return import.meta.env.VITE_prtimes;
 }
 
 export function loadMapboxToken() {
-    const data = fs.readFileSync(SECRET_TOKENS, 'utf8').trim();
-    const { token } = re_mapbox.exec(data).groups;
-
-    return token
+    return import.meta.env.VITE_mapbox;
 }
